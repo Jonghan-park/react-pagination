dddb20343bd5bddba14632942d67f76190671aa4
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-27 17:52:36 -0600

    Update fetching the data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div className="container">
       <h1>My App</h1>
